a1842144d0b4da1cad2b9df308d5f668c85103fa
felipe <felipe.conti@totvs.com.br>
2024-09-10 17:53:25 +0000

    y max

--- static/socket.js
+++ static/socket.js
@@ -91,6 +91,7 @@ function updateChart() {
           },
           y: {
             beginAtZero: true,
+            max: 100,
             ticks: {
               color: '#ffffff' // White labels
             },
